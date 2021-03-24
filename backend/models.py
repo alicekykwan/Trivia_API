46bee3864e1847d3d2014c2f7b2060abebd8d650
alicekykwan <62489895+alicekykwan@users.noreply.github.com>
2021-03-23 18:33:07 -0700

    PEP 8
    ran:
    "autopep8 --in-place --aggressive --aggressive <filename>" on models.py,  init, and test_flaskr
    checked with:
    http://pep8online.com/checkresult
    updated requirements (install of autopep8)

--- backend/models.py
+++ backend/models.py
@@ -1,16 +1,22 @@
+import json
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Integer, create_engine
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
-import os
-from sqlalchemy import Column, String, Integer, create_engine
-from flask_sqlalchemy import SQLAlchemy
-import json
 
 database_name = "trivia"
-#username and password to be configured in the .env file in the same directory or in a python terminal shell
+# username and password to be configured in the .env file in the same
+# directory or in a python terminal shell
 database_username = os.environ.get('database_username')
-database_user_pw = os.environ.get('database_user_pw') if os.environ.get('database_user_pw') else ''
-database_path = "postgres://{}:{}@{}/{}".format(database_username, database_user_pw, 'localhost:5432', database_name)
+database_user_pw = os.environ.get(
+    'database_user_pw') if os.environ.get('database_user_pw') else ''
+database_path = "postgres://{}:{}@{}/{}".format(
+    database_username,
+    database_user_pw,
+    'localhost:5432',
+    database_name)
 
 db = SQLAlchemy()
 
@@ -18,6 +24,8 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -25,60 +33,66 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
+
 '''
 Question
 
 '''
-class Question(db.Model):  
-  __tablename__ = 'questions'
 
-  id = Column(Integer, primary_key=True)
-  question = Column(String)
-  answer = Column(String)
-  category = Column(String)
-  difficulty = Column(Integer)
 
-  def __init__(self, question, answer, category, difficulty):
-    self.question = question
-    self.answer = answer
-    self.category = category
-    self.difficulty = difficulty
+class Question(db.Model):
+    __tablename__ = 'questions'
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    id = Column(Integer, primary_key=True)
+    question = Column(String)
+    answer = Column(String)
+    category = Column(String)
+    difficulty = Column(Integer)
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def __init__(self, question, answer, category, difficulty):
+        self.question = question
+        self.answer = answer
+        self.category = category
+        self.difficulty = difficulty
 
-  def format(self):
-    return {
-      'id': self.id,
-      'question': self.question,
-      'answer': self.answer,
-      'category': self.category,
-      'difficulty': self.difficulty
-    }
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'question': self.question,
+            'answer': self.answer,
+            'category': self.category,
+            'difficulty': self.difficulty
+        }
+
 
 '''
 Category
 
 '''
-class Category(db.Model):  
-  __tablename__ = 'categories'
 
-  id = Column(Integer, primary_key=True)
-  type = Column(String)
 
-  def __init__(self, type):
-    self.type = type
+class Category(db.Model):
+    __tablename__ = 'categories'
 
-  def format(self):
-    return {
-      'id': self.id,
-      'type': self.type
-    }
+    id = Column(Integer, primary_key=True)
+    type = Column(String)
+
+    def __init__(self, type):
+        self.type = type
+
+    def format(self):
+        return {
+            'id': self.id,
+            'type': self.type
+        }
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,8 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}/{}".format(
+            'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -26,20 +27,22 @@ class TriviaTestCase(unittest.TestCase):
             self.db.create_all()
 
         self.newQuestion = {
-            'question': 'What is the symbol of the element with the lowest atomic mass?',
+            'question': 'What is the symbol of the \
+             element with the lowest atomic mass?',
             'answer': 'H',
             'difficulty': 2,
-            'category': 1
-            }
+            'category': 1}
 
     def tearDown(self):
         """Executed after reach test"""
         pass
 
     """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
+    DONE
+    Write at least one test for each test for
+    successful operation and for expected errors.
     """
+
     def test_get_categories(self):
         res = self.client().get('/categories')
         data = json.loads(res.data)
@@ -55,7 +58,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['total_questions'])
         self.assertTrue(data['categories'])
         self.assertEqual(data['current_category'], None)
-    
+
     def test_404_get_questions_beyond_valid_page(self):
         res = self.client().get('/questions?page=1000')
         data = json.loads(res.data)
@@ -66,12 +69,12 @@ class TriviaTestCase(unittest.TestCase):
     def test_delete_question(self):
         res = self.client().delete('/questions/5')
         data = json.loads(res.data)
-        question = Question.query.filter(Question.id==5).one_or_none()  
+        question = Question.query.filter(Question.id == 5).one_or_none()
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['deleted'])
         self.assertEqual(question, None)
-    
+
     def test_422_delete_nonexistent_question(self):
         res = self.client().delete('/questions/100000')
         data = json.loads(res.data)
@@ -83,7 +86,8 @@ class TriviaTestCase(unittest.TestCase):
         res = self.client().post('/questions', json=self.newQuestion)
         data = json.loads(res.data)
         newQuestionID = data['created']
-        question_in_db = Question.query.filter(Question.id==newQuestionID).one_or_none()
+        question_in_db = Question.query.filter(
+            Question.id == newQuestionID).one_or_none()
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(question_in_db)
@@ -102,15 +106,18 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertTrue(data['questions'])
         self.assertEqual(len(data['questions']), 2)
-        
+
     def test_search_question_no_result(self):
-        res = self.client().post('/questions', json={'searchTerm': 'soccerball'})
+        res = self.client().post(
+            '/questions',
+            json={
+                'searchTerm': 'soccerball'})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertEqual(len(data['questions']), 0)
         self.assertEqual(data['total_questions'], 0)
-    
+
     def test_get_questions_by_category(self):
         res = self.client().get('/categories/6/questions')
         data = json.loads(res.data)
@@ -119,45 +126,52 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['total_questions'])
         self.assertTrue(data['categories'])
         self.assertEqual(data['current_category'], 6)
-    
+
     def test_422_get_questions_by_nonexistent_category(self):
         res = self.client().get('/categories/100000/questions')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 422)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], "unprocessable")
-    
+
     def test_play_quiz(self):
-        res = self.client().post('/quizzes', 
-            json={'previous_questions':[], 'quiz_category': {'id': 1}})
+        res = self.client().post(
+            '/quizzes',
+            json={
+                'previous_questions': [],
+                'quiz_category': {
+                    'id': 1}})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['question'])
 
     def test_404_play_quiz_with_nonexistent_category(self):
-        res = self.client().post('/quizzes', 
-            json={'previous_questions':[], 'quiz_category': {'id': 10000}})
+        res = self.client().post(
+            '/quizzes',
+            json={
+                'previous_questions': [],
+                'quiz_category': {
+                    'id': 10000}})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], "resource not found")
 
     def test_add_category(self):
-        res = self.client().post('/categories', 
-            json={'newCategory': 'Pop Culture'})
+        res = self.client().post('/categories',
+                                 json={'newCategory': 'Pop Culture'})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['created'])
-    
+
     def test_failed_add_category(self):
         res = self.client().post('/categories')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 422)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], "unprocessable")
-    
 
 
 # Make the tests conveniently executable
